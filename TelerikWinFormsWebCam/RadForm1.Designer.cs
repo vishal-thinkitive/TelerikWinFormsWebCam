59d1e7f04b27d6aa4946ddb00bb969804c7944c2
vishal-thinkitive <vishal.kanawade@thinkitive.com>
2023-11-03 20:34:09 +0530

    first commit

---
+++ TelerikWinFormsWebCam/RadForm1.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace TelerikWinFormsWebCam
+{
+    partial class RadForm1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            radWebCam1 = new Telerik.WinControls.UI.RadWebCam();
+            ((System.ComponentModel.ISupportInitialize)radWebCam1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)this).BeginInit();
+            SuspendLayout();
+            // 
+            // radWebCam1
+            // 
+            radWebCam1.Location = new System.Drawing.Point(127, 51);
+            radWebCam1.Name = "radWebCam1";
+            radWebCam1.Size = new System.Drawing.Size(746, 399);
+            radWebCam1.TabIndex = 0;
+            radWebCam1.Text = "radWebCam1";
+            radWebCam1.SnapshotTaken += radWebCam1_SnapshotTaken;
+            // 
+            // RadForm1
+            // 
+            AutoScaleBaseSize = new System.Drawing.Size(7, 15);
+            AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(979, 526);
+            Controls.Add(radWebCam1);
+            Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            Name = "RadForm1";
+            Text = "RadForm1";
+            Load += RadForm1_Load;
+            ((System.ComponentModel.ISupportInitialize)radWebCam1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)this).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Telerik.WinControls.UI.RadWebCam radWebCam1;
+    }
+}
---
+++ TelerikWinFormsWebCam/RadForm1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Telerik.WinControls;
+using Telerik.WinControls.UI;
+using Telerik.Windows.Documents.Spreadsheet.Expressions.Functions;
+
+namespace TelerikWinFormsWebCam
+{
+    public partial class RadForm1 : Telerik.WinControls.UI.RadForm
+    {
+        public RadForm1()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void radWebCam1_SnapshotTaken(object sender, SnapshotTakenEventArgs e)
+        {
+            string path = ConfigurationManager.AppSettings["SnapShotPath"];
+            Image snapshot = e.Snapshot;
+            snapshot.Save(path+ @"\Snap1.png");
+
+
+        }
+
+        private void RadForm1_Load(object sender, EventArgs e)
+        {
+            string path = ConfigurationManager.AppSettings["VideoPath"];
+            try
+            {
+                RadWebCam radWebCam1 = new RadWebCam();
+                radWebCam1.AutoStart = false;
+
+                radWebCam1.Start();
+                radWebCam1.RecordingFilePath = path + @"\Video1.mp4";
+                radWebCam1.StartRecording();
+
+                Task.Delay(10000).Wait();
+
+                radWebCam1.StopRecording();
+
+
+                radWebCam1.Stop();
+
+                this.Controls.Add(radWebCam1);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error occurred while starting the webcam: " + ex.Message);
+            }
+
+        }
+           
+    }
+}
